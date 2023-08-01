91b400291e66367954f128e687f61da1c7d6f508
Travis Baker <tbaker@F2N070.42adel.org.au>
2023-08-01 22:19:12 +0930

    need to fix

--- core/libft/ft_substr.c
+++ core/libft/ft_substr.c
@@ -6,13 +6,38 @@
 /*   By: tbaker <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/26 17:18:25 by tbaker            #+#    #+#             */
-/*   Updated: 2023/08/01 19:52:01 by tbaker           ###   ########.fr       */
+/*   Updated: 2023/08/01 22:18:52 by tbaker           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static	char	*ft_norm(char *substr)
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*subs;
+	size_t	i;
+
+	i = 0;
+	if (start > ft_strlen(s))
+	{
+		subs = malloc(1);
+		if (subs == NULL)
+			return (NULL);
+		subs[0] = '\0';
+		return (subs);
+	}
+	if (len > (ft_strlen(s)) - start)
+		len = (ft_strlen(s) - start);
+	while (i < len)
+	{
+		subs[i] = s[start + i];
+		i++;
+	}
+	subs[i] = '\0';
+	return (subs);
+}
+
+/*static	char	*ft_norm(char *substr)
 {
 	substr = (char *) malloc(sizeof(char) * 1);
 	if (substr == NULL)
@@ -46,7 +71,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	return (substr);
 }
 
-/*int	main(void)
+int	main(void)
 {
 	const char *s = "hello world";
 	unsigned int start = 1;
