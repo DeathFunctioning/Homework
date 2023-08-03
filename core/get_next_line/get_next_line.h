d043dab0dc902a41f5bcb9c2ba996ee312296749
deadinside <yourbrownbr3ad@gmail.com>
2023-08-03 22:57:07 +0930

    1st ring begins

---
+++ core/get_next_line/get_next_line.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbaker <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/02 19:04:27 by tbaker            #+#    #+#             */
+/*   Updated: 2023/08/02 22:59:09 by tbaker           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+# include <stdarg.h>
+
+
+
+#endif
---
+++ core/get_next_line/get_next_line_utils.c
@@ -0,0 +1,8 @@
+
+#include "get_next_line.h"
+
+int ft_some_function(char *s, int fd)
+{
+
+	return(fd);
+}
