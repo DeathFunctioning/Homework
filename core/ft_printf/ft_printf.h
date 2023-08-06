fdf472ecaa3b697a315f86a9522f52ad724e42c6
Travis Baker <tbaker@F2N069.42adel.org.au>
2023-08-06 18:52:24 +0930

    makefile making me mad

--- core/ft_printf/ft_printf.h
+++ core/ft_printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: tbaker <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/02 19:04:27 by tbaker            #+#    #+#             */
-/*   Updated: 2023/08/04 18:35:50 by tbaker           ###   ########.fr       */
+/*   Updated: 2023/08/06 17:48:20 by tbaker           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@
 # include <stdio.h>
 # include <string.h>
 # include <stdarg.h>
+# include "../libft/libft.h"
 
 int	ft_printf(const char *format, ...);
 
