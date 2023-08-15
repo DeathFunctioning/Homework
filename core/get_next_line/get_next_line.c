174dddfce7e572ddc9d0566430f3cb6235dafaf6
deadinside <yourbrownbr3ad@gmail.com>
2023-08-15 21:04:34 +0930

    End of day

--- core/get_next_line/get_next_line.c
+++ core/get_next_line/get_next_line.c
@@ -3,6 +3,27 @@
 
 char	*get_next_line(int fd)
 {
-
-	return(*s);
+	char	*line;
+	char	buffer[BUFF_MAX]	
+	
+	
+	return(line);
 }
+
+int	main(void)
+{
+	int	fd;
+	int	i;
+
+	i = 0;
+	fd = open("test.txt", O_RDONLY);
+	if (fd == -1)
+	{
+		perror("Why want this file open");
+		return (1);
+	}
+	printf("test1 = %s", get_next_line(fd));
+	close(fd);
+	return(0);
+}
+
