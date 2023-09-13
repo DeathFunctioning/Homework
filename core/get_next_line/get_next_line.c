d591b219053af9aed6671ae2166059458e690ccd
Travis Baker <tbaker@F2N069.42adel.org.au>
2023-09-13 16:44:11 +0930

    on the train

--- core/get_next_line/get_next_line.c
+++ core/get_next_line/get_next_line.c
@@ -6,42 +6,46 @@
 /*   By: tbaker <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/28 21:22:39 by tbaker            #+#    #+#             */
-/*   Updated: 2023/09/10 18:38:13 by tbaker           ###   ########.fr       */
+/*   Updated: 2023/09/13 16:43:33 by tbaker           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+char	*ft_split_buffer(char *buffer, char *line)
+{
+
+}
+
+char	*ft_copy_line(char *buffer, char *line, int bytes)
+{
+	int		i;
+	char	*temp;
+
+
+}
+
 char	*get_next_line(int fd)
 {
-	static char	buffer[BUFFER_SIZE + 1] = NULL;
+	static char	*left_over = NULL;
+	char		buffer[BUFFER_SIZE + 1];
 	char		*line;
 	int			bytes;
 
 	bytes = 1;
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	if (buffer != NULL)
-	{
-		if (ft_new_line(buffer) > 0)
-
-
-
-	}
 	while (bytes > 0)
 	{
 		bytes = read(fd, buffer, BUFFER_SIZE);
 		if (bytes < 0)
 			break;
 		buffer[bytes] = '\0';
-		if (ft_new_line(buffer) > 0)
-			buffer = ft_split_buffer(buffer);
+		if (ft_find_nl(buffer) > 0)
+			left_over = ft_split_buffer(buffer, line);
 		else
-
-			
-
-
-
+			line = ft_copy_line(buffer, line, bytes);
+	}
 	return (line);
 }
 
