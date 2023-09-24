6649fc4ea9d83fd216b24ef93d0659fceef5a231
Travis Baker <tbaker@F2N069.42adel.org.au>
2023-09-24 18:56:32 +0930

    oh no

--- core/get_next_line/get_next_line.h
+++ core/get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: tbaker <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/17 15:52:29 by tbaker            #+#    #+#             */
-/*   Updated: 2023/09/17 15:54:23 by tbaker           ###   ########.fr       */
+/*   Updated: 2023/09/24 17:58:16 by tbaker           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,8 @@
 # include <stddef.h>
 
 char	*get_next_line(int fd);
-char	*ft_strjoin(char *s1, char *s2);
-int		ft_strlen(char *s);
+char	*ft_strjoin(char const *s1, char const *s2);
+int		ft_strlen(char const *s);
 int		ft_find_nl(char *buffer);
 
 #endif
