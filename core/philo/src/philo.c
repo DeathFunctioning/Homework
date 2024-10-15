a948cf3d8f83cd40209638d1865a6f6cc4255ec7
deadinside <yourbrownbr3ad@gmail.com>
2024-10-15 12:24:22 +1030

    added test for mutex need to free malloc data once successful

--- core/philo/src/philo.c
+++ core/philo/src/philo.c
@@ -13,16 +13,25 @@
 
 // need to add destroy mutexs for forks and whatever other mutex i create
 // need to build the watcher thread to check meals eaten and for philos death :
+// need to free allocate memery?? for forks and philos if sucessful 
 int main(int argc, char *argv[])
 {
-	t_data  data;	
+	t_data  data;
+	int		i;//remove
 
+	i = 0;
 	if (argc == 5 || argc == 6)
 	{
 		if (ft_data_check(argv) == -1)
 			return (EXIT_FAILURE);
 		if (ft_init_data(&data, argc, argv) == -1)
 			return (EXIT_FAILURE);
+		//remove test for leaks make its own function for destroying mutex
+		while (i < data.philo_number)
+		{
+			pthread_mutex_destroy(&data.forks[i]);
+			i++;
+		}
 		return (EXIT_SUCCESS);
 	}
 	//need to write better error msg
