b4a887de6643f49d9ebc8258a6b228a58380679d
Travis Baker <tbaker@F2K001.42adel.org.au>
2024-10-22 19:17:02 +1030

    added cpp00

---
+++ core/cpp00/ex00/megaphone.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbaker <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/22 18:19:11 by tbaker            #+#    #+#             */
+/*   Updated: 2024/10/22 18:54:18 by tbaker           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(void)
+{
+	int x = 5;
+	int y = 5;
+	int sum = x + y;
+
+	std::cout << "Hello World!" << std::endl;
+	std::cout << "Hello World!" << std::endl;
+	std::cout << sum << std::endl;
+	return (0);
+}
