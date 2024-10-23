d9965e60abd07798e4290f9d746639f21a3179a9
Travis Baker <tbaker@F2K001.42adel.org.au>
2024-10-23 15:21:01 +1030

    almost finished the first ex00 of module cpp00

--- core/cpp00/ex00/megaphone.cpp
+++ core/cpp00/ex00/megaphone.cpp
@@ -6,20 +6,35 @@
 /*   By: tbaker <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 18:19:11 by tbaker            #+#    #+#             */
-/*   Updated: 2024/10/22 18:54:18 by tbaker           ###   ########.fr       */
+/*   Updated: 2024/10/23 14:28:57 by tbaker           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <cstring>
 
-int	main(void)
+int	main(int argc, char *argv[])
 {
-	int x = 5;
-	int y = 5;
-	int sum = x + y;
+	int	i;
+	int	j;
 
-	std::cout << "Hello World!" << std::endl;
-	std::cout << "Hello World!" << std::endl;
-	std::cout << sum << std::endl;
+	i = 0;
+	if (argc > 1)
+	{
+		i++;
+		while (i < argc)
+		{
+			j = 0;
+			while (argv[i][j])
+			{
+				putchar(toupper(argv[i][j]));
+				j++;
+			}
+			i++;
+		}
+		std::cout << std::endl;
+	}
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 	return (0);
 }
