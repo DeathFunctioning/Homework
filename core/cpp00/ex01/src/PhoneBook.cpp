8ac6e6a1ad8a41fe3227c041547d76f3fdf89b92
Travis Baker <tbaker@F2K001.42adel.org.au>
2024-11-01 17:07:05 +1030

    need to build a better index checker

--- core/cpp00/ex01/src/PhoneBook.cpp
+++ core/cpp00/ex01/src/PhoneBook.cpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbaker <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/01 12:06:40 by tbaker            #+#    #+#             */
+/*   Updated: 2024/11/01 17:04:56 by tbaker           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "PhoneBook.hpp"
 
 //test for displaying list of current contacts added 
@@ -6,33 +18,52 @@
 //i think i need to remove darkest secret and phone number ?
 PhoneBook::PhoneBook() : numberOfContacts(0), indexPosition(0) {}	
 
+bool	PhoneBook::checkIndexValue(std::string indexValue)
+{
+	if (indexValue.length() == 1)
+	{
+		if (indexValue.at(0) <= '0' && indexValue.at(0) >= '7')
+			return (true);
+		else
+		   return (false);	
+	}
+	else
+		return (false);
+}
+
 void	PhoneBook::selectContact(void)
 {
 	std::string indexValue;
 	int			nbr;
 
-	std::cout << "Enter index to view contact information: ";
+//	std::cout << "Enter index to view contact information: ";
 	while (true)
 	{
+		std::cout << "Enter index to view contact information: ";
 		std::getline(std::cin, indexValue);
 		// maybe i just cin the or i just check the first char in the string ignoring white space if not between 0 and 7 return false if its in range atoi the string might have check the string.length is only 1 bite first then check if between 0 - 7 and less than number contacts not very modulor but will work in the case
 		// i will look at it tommorow need to sleep
-		if (need build function that checks the line for valid intergeri and ruturns a bool)
-		{
-			if (nbr >= 0 && nbr < numberOfContacts)
-				break ;
-			else
-				std::cout << "Index out of range please try again: ";
-		}
+		// I NEED TO CHANGE atoi to handle errors and incorrect values 
+		if (!checkIndexValue(indexValue))
+			nbr = atoi(indexValue.c_str());// need to add error handling 
 		else
-			std::cout << "Incorrect input. Please enter index value: ";
+			std::cout << "Incorrect input.  ";
+		//if (need build function that checks the line for valid intergeri and ruturns a bool)
+		//{
+		if (nbr >= 0 && nbr < numberOfContacts)
+			break ;
+		else
+			std::cout << "Index out of range. ";
+		//}
+		//else
+		//	std::cout << "Incorrect input.  ";
 	}
 	std::cout << this->contacts[nbr].firstName << std::endl;
 	std::cout << this->contacts[nbr].lastName << std::endl;
 	std::cout << this->contacts[nbr].nickname << std::endl;
 	std::cout << this->contacts[nbr].phoneNumber << std::endl;
 	std::cout << this->contacts[nbr].darkestSecret << std::endl;
-return ;
+	return ;
 }
 
 void	PhoneBook::displayContacts(void)
@@ -61,6 +92,7 @@ void	PhoneBook::displayContacts(void)
 			std::cout << this->contacts[i].nickname;
 		std::cout << std::endl;
 	}
+	selectContact();
 	return ;
 }
 
