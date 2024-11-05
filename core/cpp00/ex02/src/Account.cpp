3d3d0f3ddfa77a7e374055ec6f2c3a51d26bb8e3
Deathfunctioning <yourbrownbr3ad@gmail.com>
2024-11-05 22:37:15 +1030

    ex02 almost completed need build displayTimestamp() and pretty up code a little. Testing done and no leaks

--- core/cpp00/ex02/src/Account.cpp
+++ core/cpp00/ex02/src/Account.cpp
@@ -1,92 +1,110 @@
 #include "Account.hpp"
 #include <iostream>
 
+
+//Need to build timestamp function currently has placeholder
+//Also need to format and pretty up code 
+
 int Account::_nbAccounts  = 0;
 int Account::_totalAmount = 0;
-int Account::_totalNbDeposists  = 0;
-int Account::_totalNbWIthdrawals  = 0;
+int Account::_totalNbDeposits  = 0;
+int Account::_totalNbWithdrawals  = 0;
 
 Account::Account(int initial_deposit)
 {
 	_accountIndex = _nbAccounts;
-	_amount = intital_deposit;
-	_nbDepoists = 0;
+	_amount = initial_deposit;
+	_nbDeposits = 0;
 	_nbWithdrawals = 0;
     _nbAccounts++;
 	_totalAmount += _amount;
-	_totalNbDeposists++;//does it count the inital depoist
-
-	std::cout << _displayTimeStamp() << " " << "index:" << _accountIndex 
-	<< ";" << "amount:" << _amount << ";" << "created" << std::endl;
+	_displayTimestamp();
+	std::cout << " index:" << _accountIndex << ";" << "amount:" << _amount << ";" << "created" << std::endl;
+	return ;
 }
 
-static int	getNbAccounts(void)
+int	Account::getNbAccounts(void)
 {
 	return(_nbAccounts);
 }
 
-static int	getTotalAmount(void)
+int	Account::getTotalAmount(void)
 {
 	return(_totalAmount);
 }
 
-static int	getNbDeposits(void)
+int	Account::getNbDeposits(void)
 {
 	return(_totalNbDeposits);
 }
 
-static int	getNbWithdrawals(void)
+int	Account::getNbWithdrawals(void)
 {
 	return(_totalNbWithdrawals);
 }
 
-static void	displayAccountInfos(void)
+void	Account::displayAccountsInfos(void)
 {
-	std::cout << _displayTimeStamp() << " " << "accounts:" << getNbAccounts() 
-		<< ";" << "total:" << getTotalAmount() << ";";
-		<< ";" << "deposit:" << getNbDeposits() << ";";
-		<< ";" << "withdrawals:" << getNbWithdrawals() << std::endl;
+	_displayTimestamp();
+	std::cout << " accounts:" << getNbAccounts() << ";" << "total:" << getTotalAmount() << ";" << ";" << "deposit:" << getNbDeposits() << ";" << ";" << "withdrawals:" << getNbWithdrawals() << std::endl;
+	return ;
 }
 
-void	makeDepoist(int depoist)
+void Account::makeDeposit(int depoist)
 {
+	_displayTimestamp();
+	std::cout << " index:" << _accountIndex << ";" << "p_amount:" << _amount << ";" << "depoist:" << depoist << ";";
 	_amount += depoist;
+	_totalAmount += depoist;
 	_nbDeposits++;
-
+	_totalNbDeposits++;
+	std::cout << "amount:" << _amount << ";" << "nb_deposits:" << getNbDeposits() << std::endl;
+	return ;
 }
 
-bool makeWithdrawal(int withdrawal)
+bool Account::makeWithdrawal(int withdrawal)
 {
 	if (_amount - withdrawal >= 0) 
 	{
+		_displayTimestamp();
+		std::cout <<  " index:" << _accountIndex << ";" << "p_amount:" << _amount << ";" << "withdrawal:" << withdrawal << ";";
 		_amount -= withdrawal;
+		_totalAmount -= withdrawal;
 		_nbWithdrawals++;
+		_totalNbWithdrawals++;
+		std::cout << "amount:" << _amount << ";" << "nb_withdrawals:" << getNbWithdrawals() << std::endl;
 		return (true);
 	}
 	else
+	{
+		_displayTimestamp();
+		std::cout <<  " index:" << _accountIndex << ";" << "p_amount:" << _amount << ";" << "withdrawal:" << "refused" << std::endl;
 		return (false);
+	}
 }
 
-int	checkAmount(void) const
+int	Account::checkAmount(void) const 
 {
 	return (0);
 }
 
-void	displayStatus(void) const
+void	Account::displayStatus(void) const
 {
-	std::cout << _displayTimeStamp() <<  " index:" << _accountIndex 
-	<< ";" << "amount:" << _amount << ";" << "depoist:" <<
-	 _amount << ";" << "withdrawals:" << std::endl;
+	_displayTimestamp();
+	std::cout <<  " index:" << _accountIndex << ";" << "amount:" << _amount << ";" << "depoist:" << _amount << ";" << "withdrawals:" << _nbWithdrawals << std::endl;
+	return ;
 }
 
-static void	_displayTimestamp(void)
+void	Account::_displayTimestamp(void)
 {
-
+	std::cout << "[19700101_000001]";//need to build real time stamp will do at 42 
+	return ;
 }
 
 Account::~Account(void)
 {
-	std::cout << _displayTimeStamp() << " index:" << _accountIndex 
-	<< ";" << "amount:" << _amount << ";" << "closed" << std::endl;
+	_displayTimestamp();
+	std::cout  << " index:" << _accountIndex << ";" << "amount:" << _amount << ";" << "closed" << std::endl;
 	_nbAccounts--;
+	return ;
 }
